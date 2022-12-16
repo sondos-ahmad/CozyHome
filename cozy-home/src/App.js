40cb9273daa8c019fc5fa98f09dd5bc5f037a8d1
yasmeen ibrahim <https://github.com/Jessy404>
2022-12-16 03:46:44 +0200

    pages

---
+++ cozy-home/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import footer from './pages/HomePage/compomants/footer';
+
+function App() {
+  return (
+    <div className="page-container">
+      <div className="content-wrap"></div>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default footer;
