6b5b22d2277214131140e2898cc69382218c12d0
mahmoud magdy <mahmoud.magdy11198@gmail.com>
2022-12-21 17:02:06 +0200

    config mongoDB file adding 3 routes (users,profile,category)

--- routes/api/category.js
+++ routes/api/category.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', (req,res) => res.send('User route'));
+router.get('/', (req,res) => res.send('Category route'));
 
 
 module.exports = router;
