15ef922a8cf3e24377efc99c179509c0dcc1bf7d
mahmoud magdy <mahmoud.magdy11198@gmail.com>
2022-12-24 18:31:33 +0200

    register user

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const db = config.get('mongoURI');
 const connectDB = async() => {
     try{
        await mongoose.connect(db,{
-        useNewUrlParser : true
+        useNewUrlParser : true,
+        
        });
        console.log("mongoDB is connected");
     } catch(err){
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,22 +1,55 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult} = require('express-validator/check');
-
+const bcrypt = require('bcryptjs');
+const {check, validationResult} = require('express-validator');
+const User = require('../../models/User');
 
 router.post('/', [
 
     check('name','Name is required').not().isEmpty(),
     check('email', 'enter valid email').isEmail(),
-    check('passwod', 'enter password with 8 or more characters').isLength({min: 8})
+    check('password', 'enter password with 8 or more characters').isLength({min: 8})
 
-],(req,res) => {
+], async (req,res) => {
     
     const err = validationResult(req);
     if(!err.isEmpty()){
         return res.status(400).json({err: err.array()});
     }
+
+    const {name,email,password} = req.body;
+
+    try{
+        let user = await User.findOne({email});
+
+        if (user){
+           return res.status(400).json({errors: [{msg:'User exists'}]});
+        }
+        
+        // making object
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        // bcrypt the password
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password,salt);
+
+        await user.save();
+
+
+        res.send('User registerd')
+
+    }catch(err){
+        console.error(err.massage);
+        res.status(500).send('server error');
+    }
     
-    res.send('User route')});
+    
+    });
 
 
 module.exports = router;
