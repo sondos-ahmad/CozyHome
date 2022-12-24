fe6e454eb1777969e335c3c4d7d0dc6485adf8f6
yasmeen ibrahim <https://github.com/Jessy404>
2022-12-24 23:42:24 +0200

    adding BedRoom and DinningRoom to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,27 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './component/layout/Navbar';
 import Landing from './component/Landing';
-import Home from './component/layout/Home'
+import Home from './component/Pages/Home'
 import Register from './component/auth/Register';
 import Login from './component/auth/Login';
-
-
+import BedRooms from './component/Pages/BedRooms';
+// import Footer from './component/layout/Footer';
+import DinningRooms from './component/Pages/DinningRooms'
 
 const App = () => (
   <>
      <Navbar />       
      <Routes><Route exact path='/'element={<Landing/>}/> </Routes>
      <Routes><Route exact path='/'element={<Home />}/> </Routes>
-    
+    {/* <Footer/> */}
+   
 
     <section className="container">
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+      <Route path='/BedRooms' element={<BedRooms />} />
+      <Route path='/DinningRooms' element={<DinningRooms />} />
       </Routes>
     </section>
   </>
