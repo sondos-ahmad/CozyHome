80868dd36d2ac456204b210e60fd9a1fd1ecc1fe
yasmeen ibrahim <https://github.com/Jessy404>
2022-12-28 21:40:58 +0200

    EGP

--- client/src/component/Pages/BedRooms.js
+++ client/src/component/Pages/BedRooms.js
@@ -27,7 +27,7 @@ export default function TitlebarBelowImageList() {
           <ImageListItemBar
         
             title={item.title}
-            subtitle={<span> Price : {item.author}</span>}
+            subtitle={<span> Price : {item.author} EGP </span>}
             position="below"
             actionIcon={
                 <IconButton
@@ -50,59 +50,59 @@ const itemData = [
   {
     img: 'https://www.ikea.com/images/a-white-hemnes-bed-in-a-bright-room-surrounded-by-sheer-whit-737c777fa87266829bd62fa39505b6ed.jpg?f=xxxl',
     title: 'white bedroom with Country-style tranquility',
-    author: '35,000 EGP',
+    author: '35,000 ',
     
   },
   {
     img: 'https://i.pinimg.com/564x/58/45/86/584586fcc86abe15e9b87a7a9f51f353.jpg',
     title: 'beige & green bedroom',
-    author: '42,935 EGP',
+    author: '42,935 ',
   },
   {
     img: 'https://i.pinimg.com/564x/51/cc/25/51cc2594d7aa9373a87e1bd9c992850b.jpg',
     title: 'Wood & Black bedroom',
-    author: ' 72,935 EGP',
+    author: ' 72,935',
   },
   {
     img: 'https://i.pinimg.com/564x/0c/10/28/0c10288da6d2eac570b049ac229bcead.jpg',
     title: 'gray bedroom',
-    author: '51,935 EGP',
+    author: '51,935 ',
   },
   {
     img: 'https://i.pinimg.com/564x/f2/b7/b6/f2b7b65dd9728a919dcac0b14595aff6.jpg',
 
     title: 'white bedroom with Genzari',
-    author: '35,000 EGP',
+    author: '35,000',
   },
   {
     img: 'https://i.pinimg.com/564x/58/9e/5b/589e5becf6175764eb13c32fdc22e889.jpg',
     title: 'woody bedroom',
-    author: '72,935 EGP',
+    author: '72,935 ',
   },
   {
     img: 'https://i.pinimg.com/564x/ab/3c/fb/ab3cfb541c37d2094f099bf678a1db6f.jpg',
     title: 'Black Bed ',
-    author: '12,935 EGP',
+    author: '12,935',
   },
   {
     img: 'https://i.pinimg.com/564x/c8/b0/61/c8b061c3d76103d9917ccff201fc7a14.jpg',
     title: 'woody bedroom',
-    author: '39,0000 EGP ',
+    author: '39,0000  ',
   },
   {
     img: 'https://i.pinimg.com/564x/58/45/86/584586fcc86abe15e9b87a7a9f51f353.jpg',
     title: 'beige & green bedroom',
-    author: '72,935 EGP',
+    author: '72,935 ',
   },
   {
     img: 'https://i.pinimg.com/564x/e7/cd/b3/e7cdb31759866f0fefe35f369a456af1.jpg',
     title: 'Woody bedroom with Country-style tranquility',
-    author: '75,000EGP',
+    author: '75,000',
   },
   {
     img: 'https://i.pinimg.com/564x/7c/19/90/7c1990badcef2684af712a0e8ff26c86.jpg',
     title: 'Modern woody bedroom',
-    author: '25,0000 EGP',
+    author: '25,0000 ',
   },
   {
     img: 'https://i.pinimg.com/564x/51/cc/25/51cc2594d7aa9373a87e1bd9c992850b.jpg',
